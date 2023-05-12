948af03b0e96fc61c5eb408e96619c27bd0fd3f5
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-05-12 12:52:06 +0530

    12-05-2023

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:income_expense_app/screen/home/view/homescreen.dart';
 import 'package:income_expense_app/screen/transactionscreen/view/transaction_screen.dart';
+import 'package:sizer/sizer.dart';
 
 void main()
 {
   runApp(
-    GetMaterialApp(
-      getPages: [
-        GetPage(name: '/',page: () => Homescreen(),),
-        GetPage(name: '/transaction',page: () => TransactionScreen(),),
-      ],
+    Sizer(
+      builder: (context, orientation, deviceType) =>  GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        getPages: [
+          GetPage(name: '/',page: () => Homescreen(),),
+          GetPage(name: '/transaction',page: () => TransactionScreen(),),
+        ],
+      ),
     ),
   );
 }
--- lib/screen/home/view/homescreen.dart
+++ lib/screen/home/view/homescreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:income_expense_app/utils/dbhelper.dart';
+import 'package:sizer/sizer.dart';
 
 class Homescreen extends StatefulWidget {
   const Homescreen({Key? key}) : super(key: key);
@@ -19,23 +21,101 @@ class _HomescreenState extends State<Homescreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
+        backgroundColor: Colors.blueGrey.shade50,
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              ElevatedButton(
-                child: Text("Insert"),
-                onPressed: () {
-                  dbHelper db = dbHelper();
-                  db.insertData(category: 'food', amount: '1000', notes: 'chill', status: 1, payType: 'online', date: '11-2-2023', time: '10:00');
-                },
+              // appbar row
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 19.sp,
+                      backgroundImage: AssetImage('assets/img/person.jpg'),
+                    ),
+                    SizedBox(width: 4.w,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Welcome !',style: GoogleFonts.varelaRound(color: Colors.blueGrey,fontSize:8.sp)),
+                        SizedBox(height: 2,),
+                        Text('Anushka Sen',style: GoogleFonts.overpass(color: Color(0xff460C68),fontWeight:FontWeight.w500,fontSize:12.sp)),
+                      ],
+                    ),
+                    Spacer(),
+                    Container(padding: EdgeInsets.all(8),decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(10)),child: Icon(Icons.notification_add,color: Color(0xff460C68),size: 18.sp,)),
+                    SizedBox(width: 4.w,),
+                  ],
+                ),
+              // total balance card
+              SizedBox(height: 2.h,),
+              Container(
+                margin: EdgeInsets.all(10),
+                height: 24.h,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xff460C68),
+                      Color(0xffCB1C8D),
+                    ]
+                  )
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Total Balance',style: GoogleFonts.overpass(color: Colors.white,fontSize: 12.sp,letterSpacing: 1)),
+                    Text('\$ 48000.00',style: GoogleFonts.overpass(color: Colors.white,fontSize: 20.sp,letterSpacing: 1)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              child: Icon(Icons.arrow_upward_rounded,color: Colors.red,size: 20.sp,),
+                              radius: 17.sp,
+                              backgroundColor: Colors.white30,
+                            ),
+                            SizedBox(width: 4.w,),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text('Expenses',style: GoogleFonts.overpass(color: Colors.white60,fontSize: 9.sp,letterSpacing: 1)),
+                                SizedBox(height: 5,),
+                                Text('800.00',style: GoogleFonts.overpass(color: Colors.white,fontSize: 12.sp,letterSpacing: 1)),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              child: Icon(Icons.arrow_downward_sharp,color: Colors.green,size: 20.sp,),
+                              radius: 17.sp,
+                              backgroundColor: Colors.white30,
+                            ),
+                            SizedBox(width: 4.w,),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text('Income',style: GoogleFonts.overpass(color: Colors.white60,fontSize: 9.sp,letterSpacing: 1)),
+                                SizedBox(height: 5,),
+                                Text('1800.00',style: GoogleFonts.overpass(color: Colors.white,fontSize: 12.sp,letterSpacing: 1)),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+
+                  ],
+                ),
               ),
-              ElevatedButton(
-                child: Text("Transaction"),
-                onPressed: () {
-                  Get.toNamed('/transaction');
-                },
-              ),
+
             ],
           ),
         ),
